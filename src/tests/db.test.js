2e0f388fb8c5acaef7a57bbec688c3d36a3fa828
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2026-09-17 15:49:52 +0000

    conserta o teste do banco de dados

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -1,18 +1,23 @@
-// ESSE ARQUIVO SERVE APENAS PARA TESTAR O BANCO DE DADOS
-// RODE COM npm test
-
 const pool = require("../database");
 
-pool.query("SELECT NOW()", (err, res) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err.stack);
-  } else {
-    console.log(
-      "Conexão bem-sucedida! Hora atual no banco de dados:",
-      res.rows[0].now,
-    );
-  }
-  pool.end();
+describe("Teste de Conexão com o Banco de Dados", () => {
+  describe("Conexão com o banco de dados", () => {
+    afterAll(async () => {
+      await pool.end();
+    });
+
+    test("Deve conectar ao banco de dados e retornar a hora atual", async () => {
+      try {
+        const result = await pool.query("SELECT NOW()");
+
+        expect(result.rows).toBeDefined();
+        expect(result.rows.length).toBeGreaterThan(0);
+
+        console.log("Conexão bem sucedida! A hora atual é:", result.rows[0].now);
+      } catch (error) {
+        console.error("Erro ao conectar ao banco de dados:", error);
+        throw error;
+      }
+    });
+  });
 });
-
-const pool = require("../database");
